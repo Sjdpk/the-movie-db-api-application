4496a5dd72ce99c6c6f964a4112f25d7c51164b3
Deepak Sapkota <sapkotad64@gmail.com>
2022-09-30 21:19:07 +0545

    paginate popular movie list

--- lib/controllers/movie.service.dart
+++ lib/controllers/movie.service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import '../congig/.env.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -7,9 +8,23 @@ import 'package:movie_app/models/movie.model.dart';
 const baseurl = "https://api.themoviedb.org/3";
 const apiKey = APIKEY;
 
+enum DataLoadingSate {
+  initial,
+  loading,
+  sucess,
+  error,
+}
+
 class MovieService with ChangeNotifier {
   bool _isLoading = false;
   bool get isLoading => _isLoading;
+
+  num _currentPage = 1;
+  num get currentPage => _currentPage;
+  set setCurretnPage(int currentPage) => _currentPage;
+
+  num _totalPage = 1;
+  num get totalPage => _totalPage;
 
   List<MovieResultsModel> _movieList = [];
   List<MovieResultsModel> get movieList => _movieList;
@@ -21,29 +36,34 @@ class MovieService with ChangeNotifier {
   set searchListSet(List<MovieResultsModel> movieList) =>
       _recentSearchList = [];
   // @desc get popular movie
-  Future getPopularMovies() async {
+  Future getPopularMovies({int currentPage = 1}) async {
     List<MovieResultsModel> movieListModelData = [];
-    List<MovieResultsModel> searchMovieListModelData = [];
+    final responseBody = await getPopularMoviesList(currentPage: currentPage);
+    final movieList = json.decode(responseBody);
+    notifyListeners();
+
+    for (var element in movieList['results']) {
+      movieListModelData.add(MovieResultsModel.fromJson(element));
+    }
+    _currentPage = movieList['page'];
+    _totalPage = movieList['total_pages'];
+    _movieList = _movieList.toSet().toList();
+    notifyListeners();
+    _movieList = [..._movieList, ...movieListModelData.toSet().toList()];
+
+    notifyListeners();
+  }
+
+  getPopularMoviesList({int currentPage = 1}) async {
     try {
-      const popularMovieUrl =
-          "/movie/popular?api_key=$apiKey&language=en-US&page=1";
-      const finalUrl = baseurl + popularMovieUrl;
+      final popularMovieUrl =
+          "/movie/popular?api_key=$apiKey&language=en-US&page=$currentPage";
+      final finalUrl = baseurl + popularMovieUrl;
       final response = await http.get(Uri.parse(finalUrl));
       if (response.statusCode == 200) {
-        final movieList = json.decode(response.body);
-        notifyListeners();
-
-        for (var element in movieList['results']) {
-          movieListModelData.add(MovieResultsModel.fromJson(element));
-        }
-        _movieList = movieListModelData;
-
-        notifyListeners();
-      } else {
-        _isLoading = true;
+        return response.body;
       }
     } catch (e) {
-      _isLoading = true;
       rethrow;
     }
   }
--- lib/screens/homepage.screen.dart
+++ lib/screens/homepage.screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:movie_app/models/movie.model.dart';
 import 'package:movie_app/screens/moviedetails.screen.dart';
@@ -39,151 +41,182 @@ class MoviesHomePage extends StatelessWidget {
         elevation: 1,
         title: displayText("The Movie"),
       ),
-      body: SingleChildScrollView(
-          padding: const EdgeInsets.all(14),
-          child: Consumer<SearchController>(
-            builder: (context, searchCtr, child) {
-              return searchPvr.namesList.isEmpty
-                  ? Container(
-                      margin: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height / 2.5,
-                      ),
-                      alignment: Alignment.center,
-                      child: const CircularProgressIndicator(
-                        backgroundColor: Colors.black,
-                        color: Colors.white,
-                      ),
-                    )
-                  : Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Flexible(
-                              child: Container(
-                                margin: const EdgeInsets.only(right: 10),
-                                height: 40,
-                                decoration: decoration,
-                                child: TextFormField(
-                                  controller: searchController,
-                                  cursorColor: Colors.black,
-                                  onChanged: (value) {
-                                    searchCtr.updateSearchText(value);
-                                    List<MovieResultsModel> newList = [];
+      body: NotificationListener<ScrollEndNotification>(
+        onNotification: (scrollEnd) {
+          final metrics = scrollEnd.metrics;
+          if (metrics.atEdge) {
+            bool isTop = metrics.pixels == 0;
+            if (isTop) {
+              log("scroll to  top");
+            } else {
+              if (movieList.currentPage < movieList.totalPage) {
+                int currentPage = movieList.currentPage.toInt() + 1;
+                // context
+                //     .read<MovieService>()
+                //     .getPopularMovies(currentPage: currentPage);
+                context
+                    .read<MovieService>()
+                    .getPopularMovies(currentPage: currentPage)
+                    .then((_) {
+                  Future.delayed(Duration.zero, () {
+                    searchPvr.updateSearchList(movieList.movieList);
+                    searchPvr.namesList = movieList.movieList;
+                  });
+                });
+                log("scroll to  bottom ${movieList.currentPage} ${movieList.totalPage}");
+              } else {
+                log("no more data to load");
+              }
+            }
+          }
+          return true;
+        },
+        child: SingleChildScrollView(
+            padding: const EdgeInsets.all(14),
+            child: Consumer<SearchController>(
+              builder: (context, searchCtr, child) {
+                return searchPvr.namesList.isEmpty
+                    ? Container(
+                        margin: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height / 2.5,
+                        ),
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(
+                          backgroundColor: Colors.black,
+                          color: Colors.white,
+                        ),
+                      )
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Flexible(
+                                child: Container(
+                                  margin: const EdgeInsets.only(right: 10),
+                                  height: 40,
+                                  decoration: decoration,
+                                  child: TextFormField(
+                                    controller: searchController,
+                                    cursorColor: Colors.black,
+                                    onChanged: (value) {
+                                      searchCtr.updateSearchText(value);
+                                      List<MovieResultsModel> newList = [];
 
-                                    for (var i = 0;
-                                        i < searchCtr.namesList.length;
-                                        i++) {
-                                      if (searchCtr.namesList[i].title!
-                                          .toLowerCase()
-                                          .toLowerCase()
-                                          .contains(value.toLowerCase())) {
-                                        newList.add(searchCtr.namesList[i]);
+                                      for (var i = 0;
+                                          i < searchCtr.namesList.length;
+                                          i++) {
+                                        if (searchCtr.namesList[i].title!
+                                            .toLowerCase()
+                                            .toLowerCase()
+                                            .contains(value.toLowerCase())) {
+                                          newList.add(searchCtr.namesList[i]);
+                                        }
                                       }
-                                    }
-                                    searchCtr.updateSearchList(newList);
-                                    if (value == "") {
-                                      searchCtr.updateSearchList(
-                                          searchCtr.namesList);
-                                    }
-                                  },
-                                  decoration: InputDecoration(
-                                    suffixIcon: IconButton(
-                                      splashRadius: 0.1,
-                                      onPressed: () {
-                                        searchController.clear();
-                                        searchCtr.updateSearchText("");
+                                      searchCtr.updateSearchList(newList);
+                                      if (value == "") {
                                         searchCtr.updateSearchList(
                                             searchCtr.namesList);
-                                      },
-                                      icon: Icon(
-                                        Icons.clear,
-                                        color: Colors.black,
-                                        size: searchController.text != ""
-                                            ? 20
-                                            : 0,
+                                      }
+                                    },
+                                    decoration: InputDecoration(
+                                      suffixIcon: IconButton(
+                                        splashRadius: 0.1,
+                                        onPressed: () {
+                                          searchController.clear();
+                                          searchCtr.updateSearchText("");
+                                          searchCtr.updateSearchList(
+                                              searchCtr.namesList);
+                                        },
+                                        icon: Icon(
+                                          Icons.clear,
+                                          color: Colors.black,
+                                          size: searchController.text != ""
+                                              ? 20
+                                              : 0,
+                                        ),
                                       ),
+                                      hintText: 'Search',
+                                      contentPadding: const EdgeInsets.only(
+                                        top: 7,
+                                        bottom: 10,
+                                        left: 10,
+                                        right: 10,
+                                      ),
+                                      border: InputBorder.none,
                                     ),
-                                    hintText: 'Search',
-                                    contentPadding: const EdgeInsets.only(
-                                      top: 7,
-                                      bottom: 10,
-                                      left: 10,
-                                      right: 10,
-                                    ),
-                                    border: InputBorder.none,
                                   ),
                                 ),
                               ),
-                            ),
-                            InkWell(
-                              onTap: () async {
-                                Navigator.pushNamed(context, '/search');
-                              },
-                              child: Container(
-                                height: 40,
-                                padding: const EdgeInsets.all(7),
-                                decoration: decoration,
-                                child: const Icon(Icons.search),
-                              ),
-                            )
-                          ],
-                        ),
-                        displayText(
-                          searchCtr.searchText == ""
-                              ? "Popular Movies"
-                              : "Movie Searches",
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          bottomPadding: 14,
-                          topPadding: 14,
-                        ),
-                        searchCtr.searchList.isNotEmpty
-                            ? GridView.builder(
-                                shrinkWrap: true,
-                                physics: const ScrollPhysics(),
-                                gridDelegate:
-                                    const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  mainAxisExtent: 192,
-                                  mainAxisSpacing: 16,
-                                  crossAxisSpacing: 16,
-                                ),
-                                itemCount: searchCtr.searchList.length,
-                                itemBuilder: (context, index) {
-                                  return InkWell(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (_) => MovieDetailsScreen(
-                                            movieResultsModel:
-                                                searchCtr.searchList[index],
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                    child: displaymovieCard(
-                                      image:
-                                          "https://image.tmdb.org/t/p/w500/${searchCtr.searchList[index].backdropPath}",
-                                      moviename:
-                                          searchCtr.searchList[index].title,
-                                    ),
-                                  );
+                              InkWell(
+                                onTap: () async {
+                                  Navigator.pushNamed(context, '/search');
                                 },
-                              )
-                            : Center(
-                                child: displayText(
-                                  "No Movie Found 🧐 ",
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  topPadding: 30,
+                                child: Container(
+                                  height: 40,
+                                  padding: const EdgeInsets.all(7),
+                                  decoration: decoration,
+                                  child: const Icon(Icons.search),
                                 ),
-                              ),
-                      ],
-                    );
-            },
-          )),
+                              )
+                            ],
+                          ),
+                          displayText(
+                            searchCtr.searchText == ""
+                                ? "Popular Movies"
+                                : "Movie Searches",
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            bottomPadding: 14,
+                            topPadding: 14,
+                          ),
+                          searchCtr.searchList.isNotEmpty
+                              ? GridView.builder(
+                                  shrinkWrap: true,
+                                  physics: const ScrollPhysics(),
+                                  gridDelegate:
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    mainAxisExtent: 192,
+                                    mainAxisSpacing: 16,
+                                    crossAxisSpacing: 16,
+                                  ),
+                                  itemCount: searchCtr.searchList.length,
+                                  itemBuilder: (context, index) {
+                                    return InkWell(
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (_) => MovieDetailsScreen(
+                                              movieResultsModel:
+                                                  searchCtr.searchList[index],
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                      child: displaymovieCard(
+                                        image:
+                                            "https://image.tmdb.org/t/p/w500/${searchCtr.searchList[index].backdropPath}",
+                                        moviename:
+                                            searchCtr.searchList[index].title,
+                                      ),
+                                    );
+                                  },
+                                )
+                              : Center(
+                                  child: displayText(
+                                    "No Movie Found 🧐 ",
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    topPadding: 30,
+                                  ),
+                                ),
+                        ],
+                      );
+              },
+            )),
+      ),
     );
   }
 }
